e4f80de20cb4c36ca62b4c20c86d7764f515b37c
alexcueto88 <acueto@acceptance.com>
2023-04-24 20:30:44 -0500

    Added Service Model

---
+++ GigHub/Models/Service.cs
@@ -0,0 +1,15 @@
+﻿namespace GigHub.Models
+{
+    public class Service
+    {
+        public string id { get; set; }
+        
+        public string UserRoleId { get; set; }
+        
+        public string ServiceDescription { get; set; }
+
+        public int ServiceRate { get; set; }    
+
+
+    }
+}
