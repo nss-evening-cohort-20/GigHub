57d8b588a2b03787ce8b009629fe9f35c5e628cb
Thomas Kibby <107074127+htkibby@users.noreply.github.com>
2023-04-25 20:06:41 -0500

    Revert "Thomas gig hub"

--- GigHub/Program.cs
+++ GigHub/Program.cs
@@ -1,5 +1,3 @@
-using GigHub.Repositories;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,7 +6,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddTransient<IUserRepository, UserRepository>();
 
 var app = builder.Build();
 
