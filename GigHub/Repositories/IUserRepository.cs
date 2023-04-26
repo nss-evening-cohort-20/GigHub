aad22ee982bdfaaf32037ae78f04d06acb5710df
Thomas Kibby <htkibby@gmail.com>
2023-04-25 20:59:58 -0500

    Remade user repo combo

---
+++ GigHub/Repositories/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using GigHub.Models;
+
+namespace GigHub.Repositories
+{
+    public interface IUserRepository
+    {
+        void Delete(int id);
+        List<User> GetAllUsers();
+        User GetUserById(int id);
+        void Insert(User user);
+        void Update(User user);
+    }
+}
