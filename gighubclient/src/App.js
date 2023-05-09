eee9264421285b4753597c8dd2604afcb150f747
dhollifield <deannahollifield@gmail.com>
2023-05-08 20:08:34 -0500

    added more base/venue css, js

--- gighubclient/src/App.js
+++ gighubclient/src/App.js
@@ -2,13 +2,14 @@ import logo from "./components/images/GigHub_logo.jpg";
 import "./App.css";
 import React from "react";
 import { VenueProvider } from "./providers/VenueProvider";
-import VenueList from "./components/VenueList";
+import VenueList from "./components/venue/VenueList";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1 className="App-title">GigHub</h1>
       </header>
       <div>
         <VenueProvider>
